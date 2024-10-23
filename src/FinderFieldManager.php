c5299757424822942f57cef3ff4b6a7c520df807
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-10-23 10:21:34 +0100

    Added Finder field manager service.

---
+++ src/FinderFieldManager.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\localgov_finders;
+
+use Drupal\Core\Config\ConfigInstallerInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleExtensionList;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\search_api\Utility\PluginHelperInterface;
+
+/**
+ * TODO: class docs.
+ */
+class FinderFieldManager {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The entity field manager.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManagerInterface
+   */
+  protected $entityFieldManager;
+
+  /**
+   * The module extension list service.
+   *
+   * @var \Drupal\Core\Extension\ModuleExtensionList
+   */
+  protected $moduleExtensionList;
+
+  /**
+   * The config installer service.
+   *
+   * @var \Drupal\Core\Config\ConfigInstallerInterface
+   */
+  protected $configInstaller;
+
+  /**
+   * The logger channel factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerChannelFactory;
+
+  /**
+   * The plugin helper service.
+   *
+   * @var \Drupal\search_api\Utility\PluginHelperInterface
+   */
+  protected $pluginHelper;
+
+  /**
+   * Creates a FinderFieldManager instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   The entity field manager.
+   * @param \Drupal\Core\Extension\ModuleExtensionList $module_extension_list
+   *   The module extension list service.
+   * @param \Drupal\Core\Config\ConfigInstallerInterface $config_installer
+   *   The config installer service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_channel_factory
+   *   The logger channel factory.
+   * @param \Drupal\search_api\Utility\PluginHelperInterface $plugin_helper
+   *   The plugin helper service.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entity_type_manager,
+    EntityFieldManagerInterface $entity_field_manager,
+    ModuleExtensionList $module_extension_list,
+    ConfigInstallerInterface $config_installer,
+    LoggerChannelFactoryInterface $logger_channel_factory,
+    PluginHelperInterface $plugin_helper,
+  ) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->entityFieldManager = $entity_field_manager;
+    $this->moduleExtensionList = $module_extension_list;
+    $this->configInstaller = $config_installer;
+    $this->loggerChannelFactory = $logger_channel_factory;
+    $this->pluginHelper = $plugin_helper;
+  }
+
+}
