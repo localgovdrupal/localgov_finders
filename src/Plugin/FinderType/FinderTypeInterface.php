aac9ed7ade9217566ee3b45aea25acf39239225c
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-10-23 16:24:57 +0100

    add method to interface

--- src/Plugin/FinderType/FinderTypeInterface.php
+++ src/Plugin/FinderType/FinderTypeInterface.php
@@ -4,10 +4,22 @@ namespace Drupal\localgov_finders\Plugin\FinderType;
 
 use Drupal\Component\Plugin\DerivativeInspectionInterface;
 use Drupal\Component\Plugin\PluginInspectionInterface;
+use Drupal\node\NodeTypeInterface;
 
 /**
  * Interface for Finder Type plugins.
  */
 interface FinderTypeInterface extends PluginInspectionInterface, DerivativeInspectionInterface {
+
+  /**
+   * Gets the bundle field definitions for a finder channel node type.
+   *
+   * @param \Drupal\node\NodeTypeInterface $node_type
+   *   The node type.
+   *
+   * @return array
+   *   An array of field definitions, keyed by the field name.
+   */
+  public function getChannelFieldDefinitions(NodeTypeInterface $node_type): array;
 
 }
