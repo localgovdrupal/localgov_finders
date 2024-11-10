fc68671dcd6893dbcb287368e85e916f3710f8aa
ekes <ekes@iskra.net>
2024-11-10 11:28:01 +0100

    Add a non-node entry plugin (geo as that's the example that is out there already).

---
+++ modules/finders_geo/src/Plugin/FinderType/Geo.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Drupal\finders_geo\Plugin\FinderType;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\finders\Attribute\FinderType;
+use Drupal\finders\Field\BundleFieldDefinition;
+use Drupal\finders\Plugin\FinderType\FinderTypeBase;
+use Drupal\search_api\IndexInterface;
+
+/**
+ * Finder type for geo.
+ *
+ * Provides calendars and listings.
+ *
+ * Geo entries use the date_recur SearchAPI datasource, which adds an index
+ * item for each occurrence of a recurring date rather than for each node.
+ */
+#[FinderType(
+  id: "geo",
+  label: new TranslatableMarkup("geo"),
+  description: new TranslatableMarkup("Provides geo listings and maps"),
+)]
+class Geo extends FinderTypeBase {
+
+  /**
+   * The field name for the list view field.
+   *
+   * @see self::getListViewFieldDefinition()
+   */
+  const LIST_VIEW_FIELD = 'localgov_geo_list_view';
+
+  /**
+   * The field name for the map view field.
+   *
+   * @see self::getListViewFieldDefinition()
+   */
+  const MAP_VIEW_FIELD = 'localgov_geo_map_view';
+
+  /**
+   * The field name for the geo location field.
+   *
+   * @see self::getGeoLocationFieldDefinition()
+   */
+  const GEO_FIELD = 'location';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIndexIds(): array {
+    return [
+      'finders_index_geo',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getChannelFieldDefinitions(ConfigEntityInterface $bundle): array {
+    $field_definitions = parent::getChannelFieldDefinitions($bundle);
+
+    $list_view_field = $this->getListViewFieldDefinition($bundle);
+    $list_view_field->setTargetBundle($bundle->id());
+    $field_definitions[$list_view_field->getName()] = $list_view_field;
+
+    $map_view_field = $this->getMapViewFieldDefinition($bundle);
+    $map_view_field->setTargetBundle($bundle->id());
+    $field_definitions[$map_view_field->getName()] = $map_view_field;
+
+    return $field_definitions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterIndexFields(IndexInterface $index, ConfigEntityInterface $entry_bundle_entity): void {
+    parent::alterIndexFields($index, $entry_bundle_entity);
+
+    // Get the entity type ID of the entry entities that the entry bundle entity
+    // defines.
+    $entry_entity_type_id = $entry_bundle_entity->getEntityType()->getBundleOf();
+    $datasource_id = $this->getIndexDatasourceId($index, $entry_entity_type_id);
+    $datasource = $index->getDatasource($datasource_id);
+
+    // @todo add location search fields.
+  }
+
+  /**
+   * Gets the definition for the list view selection field.
+   *
+   * This field on channels controls allows selecting the view to show a listing
+   * of geo entries.
+   *
+   * @param \Drupal\Core\Config\Entity\ConfigEntityInterface $bundle
+   *   The bundle entity.
+   *
+   * @return \Drupal\finders\Field\BundleFieldDefinition
+   *   The bundle field definition.
+   */
+  protected function getListViewFieldDefinition(ConfigEntityInterface $bundle): BundleFieldDefinition {
+    $bundle_entity_type = $bundle->getEntityType();
+    $content_entity_type_id = $bundle_entity_type->getBundleOf();
+
+    return BundleFieldDefinition::create('viewsreference')
+      ->setName(static::LIST_VIEW_FIELD)
+      ->setTargetEntityTypeId($content_entity_type_id)
+      ->setLabel(t('Geo list view'))
+      ->setRequired(FALSE)
+      ->setTranslatable(FALSE)
+      ->setCardinality(1)
+      ->setSettings([
+        'target_type' => 'view',
+        'handler' => 'finders_channel_views',
+        'handler_settings' => [
+          'target_bundles' => NULL,
+          'auto_create' => FALSE,
+        ],
+        'plugin_types' => [
+          'embed' => 'embed',
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'viewsreference_select',
+      ]);
+  }
+
+  /**
+   * Gets the definition for the list view selection field.
+   *
+   * This field on channels controls allows selecting the view to show a listing
+   * of geo entries.
+   *
+   * @param \Drupal\Core\Config\Entity\ConfigEntityInterface $bundle
+   *   The bundle entity.
+   *
+   * @return \Drupal\finders\Field\BundleFieldDefinition
+   *   The bundle field definition.
+   */
+  protected function getMapViewFieldDefinition(ConfigEntityInterface $bundle): BundleFieldDefinition {
+    $bundle_entity_type = $bundle->getEntityType();
+    $content_entity_type_id = $bundle_entity_type->getBundleOf();
+
+    return BundleFieldDefinition::create('viewsreference')
+      ->setName(static::MAP_VIEW_FIELD)
+      ->setTargetEntityTypeId($content_entity_type_id)
+      ->setLabel(t('Geo map view'))
+      ->setRequired(FALSE)
+      ->setTranslatable(FALSE)
+      ->setCardinality(1)
+      ->setSettings([
+        'target_type' => 'view',
+        'handler' => 'finders_channel_views',
+        'handler_settings' => [
+          'target_bundles' => NULL,
+          'auto_create' => FALSE,
+        ],
+        'plugin_types' => [
+          'embed' => 'embed',
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'viewsreference_select',
+      ]);
+  }
+
+}
---
+++ modules/finders_geo/tests/src/Kernel/GeoFinderPluginTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Drupal\Tests\finders_geo\Kernel;
+
+use Drupal\finders_geo\Plugin\FinderType\Geo;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\finders\Enum\FinderRole;
+use Drupal\finders\Plugin\FinderType\FinderTypeBase;
+use Drupal\search_api\Entity\Index;
+
+/**
+ * Tests the Geo finder type plugin.
+ *
+ * @group finders
+ */
+class GeoFinderPluginTest extends KernelTestBase {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'system',
+    'user',
+    'views',
+    'node',
+    'finders',
+    'finders_geo',
+    'search_api',
+    'viewsreference',
+    'geofield',
+    'geo_entity',
+    // @todo make a geo_entity_test
+    // and then move location into a basefield!
+    'geo_entity_area',
+    // Oof and geo_entity has a set of config dependencies.
+    'entity_browser',
+    'entity_browser_entity_form'
+  ];
+
+  // Disable config checking -- the schema for Finders third-party settings on
+  // the entity_test_with_bundle entity type is in the test module and we don't
+  // want what it installs.
+  protected $strictConfigSchema = FALSE;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    parent::setUp();
+
+    $this->installEntitySchema('search_api_task');
+    $this->installEntitySchema('node');
+    $this->installEntitySchema('geo_entity');
+    $this->installConfig('geo_entity');
+    $this->installConfig('geo_entity_area');
+
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+  }
+
+  /**
+   * Tests event finder type.
+   */
+  public function testEventFinderType() {
+    // Create a channel bundle.
+    $channel_bundle = $this->entityTypeManager->getStorage('node_type')->create([
+      'type' => 'test_channel_bundle',
+      'status' => TRUE,
+    ]);
+    $channel_bundle->setThirdPartySetting(
+      'finders',
+      'finder_type',
+      'geo'
+    );
+    $channel_bundle->setThirdPartySetting(
+      'finders',
+      'finder_role',
+      FinderRole::Channel->value,
+    );
+    $channel_bundle->save();
+
+    // Update the geo type to be an entry.
+    $entry_bundle = $this->entityTypeManager->getStorage('geo_entity_type')->load('area');
+    $entry_bundle->setThirdPartySetting(
+      'finders',
+      'finder_type',
+      'geo'
+    );
+    $entry_bundle->setThirdPartySetting(
+      'finders',
+      'finder_role',
+      FinderRole::Entries->value,
+    );
+    $entry_bundle->save();
+
+    // The channel and entry bundles have finder fields defined on them.
+    $entity_field_manager = \Drupal::service('entity_field.manager');
+
+    $channel_fields = $entity_field_manager->getFieldDefinitions('node', 'test_channel_bundle');
+    $this->assertArrayHasKey(FinderTypeBase::CHANNEL_TYPES_FIELD, $channel_fields);
+    $this->assertArrayHasKey(Geo::LIST_VIEW_FIELD, $channel_fields);
+    $this->assertArrayHasKey(Geo::MAP_VIEW_FIELD, $channel_fields);
+
+    $geo_fields = $entity_field_manager->getFieldDefinitions('geo_entity', 'area');
+    $this->assertArrayHasKey(FinderTypeBase::CHANNEL_SELECTION_FIELD, $geo_fields);
+
+    // A search index has been created by the creation of the channel bundle.
+    $search_index = Index::load('finders_index_geo');
+    $this->assertNotEmpty($search_index);
+
+    $datasources = $search_index->getDatasources();
+    $this->assertCount(1, $datasources);
+    $datasource = reset($datasources);
+    $this->assertEquals('geo_entity', $datasource->getDerivativeId());
+    $this->assertContains('Area', $datasource->getBundles());
+
+    $fields = $search_index->getFields();
+    #$this->assertArrayHasKey('title', $fields);
+    #$this->assertArrayHasKey('rendered_item', $fields);
+    #$this->assertArrayHasKey('finders_title_sort', $fields);
+    #$this->assertArrayHasKey('finders_channels', $fields);
+    // @todo then the location field(s).
+  }
+
+}
