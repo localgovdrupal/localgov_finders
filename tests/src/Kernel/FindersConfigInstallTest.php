d339294fa2340cff9ffb1769920544045070847b
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-11-13 14:39:15 +0000

    added separate test for installation from config

---
+++ tests/src/Kernel/FindersConfigInstallTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\Tests\finders\Kernel;
+
+use Drupal\finders\Plugin\FinderType\FinderTypeBase;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests installing a finder from install config.
+ *
+ * @group finders
+ */
+class FindersConfigInstallTest extends KernelTestBase {
+
+  /**
+   * The modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'system',
+    'user',
+    'views',
+    'viewsreference',
+    'search_api',
+    'finders',
+    'finders_test',
+    'entity_test',
+  ];
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->installEntitySchema('search_api_task');
+
+    $this->installConfig('finders_test');
+    $this->installEntitySchema('entity_test_with_bundle');
+
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+  }
+
+  /**
+   * Tests installation of a finder from module install config.
+   */
+  public function testModuleConfig(): void {
+    $channel = $this->entityTypeManager->getStorage('entity_test_with_bundle')->create([
+      'name' => 'test channel',
+      'type' => 'finders_channel',
+    ]);
+    $channel->save();
+    $this->assertTrue($channel->hasField(FinderTypeBase::CHANNEL_TYPES_FIELD));
+  }
+
+}
