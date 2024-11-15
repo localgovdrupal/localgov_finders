fa2fc440e263333ecabd4d2e4fcd9550b8466785
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-11-15 17:09:30 +0000

    POC field upgrade test

--- tests/src/Kernel/FinderPluginTest.php
+++ tests/src/Kernel/FinderPluginTest.php
@@ -12,6 +12,9 @@ use Drupal\localgov_finders\Enum\FinderRole;
 use Drupal\localgov_finders\Plugin\FinderType\FinderTypeBase;
 use Drupal\search_api\Entity\Index;
 use Hoa\File\Finder;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\Tests\field\Traits\EntityReferenceFieldCreationTrait;
 
 /**
  * Test description.
@@ -19,6 +22,8 @@ use Hoa\File\Finder;
  * @group localgov_finders
  */
 final class FinderPluginTest extends KernelTestBase {
+
+  use EntityReferenceFieldCreationTrait;
 
   /**
    * The entity type manager service.
@@ -35,7 +40,8 @@ final class FinderPluginTest extends KernelTestBase {
     'localgov_finders_test',
     'search_api',
     'entity_test',
-    'user'
+    'user',
+    'field',
   ];
 
   /**
@@ -46,10 +52,106 @@ final class FinderPluginTest extends KernelTestBase {
 
     $this->installEntitySchema('search_api_task');
 
-    $this->installConfig('localgov_finders_test');
+    // $this->installConfig('localgov_finders_test');
     $this->installEntitySchema('entity_test_with_bundle');
 
     $this->entityTypeManager = $this->container->get('entity_type.manager');
+  }
+
+
+    // This breaks because of the channel bundle that's in the config install! ARGH!
+    // But that's ok -- the upgrade path won't have config imported.
+
+
+  public function testUpgrade(): void {
+    $channel_bundle = EntityTestBundle::create([
+      'id' => 'test_channel_bundle',
+      'status' => TRUE,
+    ]);
+    $channel_bundle->save();
+
+    $entry_bundle_one = EntityTestBundle::create([
+      'id' => 'test_entry_bundle_one',
+      'status' => TRUE,
+    ]);
+    $entry_bundle_one->save();
+
+    $this->createEntityReferenceField('entity_test_with_bundle', 'test_channel_bundle', FinderTypeBase::CHANNEL_TYPES_FIELD, 'label', 'entity_test_with_bundle', cardinality: -1);
+
+    $entry = EntityTestWithBundle::create([
+      'name' => 'test channel',
+      'type' => 'test_entry_bundle_one',
+    ]);
+    $entry->save();
+
+    $channel = EntityTestWithBundle::create([
+      'name' => 'test channel',
+      'type' => 'test_channel_bundle',
+      FinderTypeBase::CHANNEL_TYPES_FIELD => [$entry->id()],
+    ]);
+    $channel->save();
+    $this->assertTrue($channel->hasField(FinderTypeBase::CHANNEL_TYPES_FIELD));
+    $this->assertFalse($channel->get(FinderTypeBase::CHANNEL_TYPES_FIELD)->isEmpty());
+    // dump($channel->toArray());
+    //
+
+    $entity_field_manager = \Drupal::service('entity_field.manager');
+    $table_mapping = $this->entityTypeManager->getStorage('entity_test_with_bundle')->getTableMapping();
+    $field_definition = $entity_field_manager->getFieldStorageDefinitions('entity_test_with_bundle')[FinderTypeBase::CHANNEL_TYPES_FIELD];
+    $table_name = $table_mapping->getDedicatedDataTableName($field_definition);
+
+    $database = \Drupal::service('database');
+    $database->query('CREATE TABLE {backup} AS SELECT * FROM {' . $table_name . '}');
+    // TODO! revision table backup!
+
+    FieldStorageConfig::loadByName('entity_test_with_bundle', FinderTypeBase::CHANNEL_TYPES_FIELD)->delete();
+    // TODO: need to purge them ALL?!
+    // TODO -- the previous call renames the DB table, so do we even need this??
+    // field_purge_batch(10);
+
+    // Set both bundles as finders.
+    $channel_bundle->setThirdPartySetting(
+      'localgov_finders',
+      'finder_type',
+      'test'
+    );
+    $channel_bundle->setThirdPartySetting(
+      'localgov_finders',
+      'finder_role',
+      'channel'
+    );
+    $channel_bundle->save();
+
+    // Set up the entry bundle as finder entries.
+    $entry_bundle_one->setThirdPartySetting(
+      'localgov_finders',
+      'finder_type',
+      'test'
+    );
+    $entry_bundle_one->setThirdPartySetting(
+      'localgov_finders',
+      'finder_role',
+      FinderRole::Entries->value,
+    );
+    $entry_bundle_one->save();
+
+    // Need to get the new field definition, as it won't have the same unique
+    // ID which gets used to make the hash if the table name has one.
+    $field_definition = $entity_field_manager->getFieldStorageDefinitions('entity_test_with_bundle')[FinderTypeBase::CHANNEL_TYPES_FIELD];
+    $table_name = $table_mapping->getDedicatedDataTableName($field_definition);
+    $database->query('INSERT INTO {' . $table_name . '} SELECT * FROM {backup}');
+    // TODO delete the temp table for the next field to work!
+
+    $channel = $this->reloadEntity($channel);
+    $this->assertTrue($channel->hasField(FinderTypeBase::CHANNEL_TYPES_FIELD));
+    $this->assertFalse($channel->get(FinderTypeBase::CHANNEL_TYPES_FIELD)->isEmpty());
+
+    // INSERT INTO new_table SELECT * FROM old_table;
+
+    // dump($channel->toArray());
+    // Nooooo this deleted our field :(
+    // dump($channel->getFieldDefinition(FinderTypeBase::CHANNEL_TYPES_FIELD));
+
   }
 
   /**
