48305834b943f81b5bd1bcc7cf880fcbd381896d
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-11-18 10:02:48 +0000

    added facets to finder form

---
+++ modules/finders_facets/src/Hook/FindersFormAlterHooks.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\finders_facets\Hook;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Render\Element;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Contains form alter hook implementations for the Finders facets module.
+ */
+class FindersFormAlterHooks {
+
+  use StringTranslationTrait;
+
+  /**
+   * Implements hook_form_FORM_ID_alter().
+   */
+  #[Hook('form_finder_form_alter')]
+  public function formFinderFormAlter(&$form, FormStateInterface $form_state, $form_id) {
+    $finder = $form_state->getFormObject()->getEntity();
+
+    $form['facets'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable facets'),
+      '#default_value' => $finder->getThirdPartySetting('finders_facets', 'facets', FALSE),
+    ];
+
+    foreach (Element::children($form['actions']) as $action) {
+      $form['actions'][$action]['#validate'][] = static::class . '::validate';
+      $form['actions'][$action]['#submit'][] = static::class . '::submit';
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function validate(array $form, FormStateInterface $form_state): void {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function submit(array $form, FormStateInterface $form_state): void {
+    $finder = $form_state->getFormObject()->getEntity();
+
+    if (empty($form_state->getValue(['facets']))) {
+      $finder->unsetThirdPartySetting(
+        'finders_facets',
+        'facets',
+      );
+    }
+    else {
+      $finder->setThirdPartySetting(
+        'finders_facets',
+        'facets',
+        $form_state->getValue(['facets'])
+      );
+      $finder->save();
+    }
+  }
+
+}
