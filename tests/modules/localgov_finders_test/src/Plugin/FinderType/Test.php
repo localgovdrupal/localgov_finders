b572c62d426d3c86587e37cbdec50bf1926f59a8
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-11-20 10:42:22 +0000

    remove unused code

--- tests/modules/localgov_finders_test/src/Plugin/FinderType/Test.php
+++ tests/modules/localgov_finders_test/src/Plugin/FinderType/Test.php
@@ -2,12 +2,8 @@
 
 namespace Drupal\finders_test\Plugin\FinderType;
 
-use Drupal\Core\Config\Entity\ConfigEntityTypeInterface;
-use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\finders\Attribute\FinderType;
-use Drupal\finders\Constants\FinderField;
-use Drupal\finders\Field\BundleFieldDefinition;
 use Drupal\finders\Plugin\FinderType\FinderTypeBase;
 
 /**
@@ -19,24 +15,5 @@ use Drupal\finders\Plugin\FinderType\FinderTypeBase;
   description: new TranslatableMarkup("Provides dummy plugin for testing"),
 )]
 class Test extends FinderTypeBase {
-/*
-  protected function getChannelTypesFieldDefinition(ConfigEntityTypeInterface $entity_type): FieldDefinitionInterface {
-    return BundleFieldDefinition::create('entity_reference')
-      ->setName(FinderField::CHANNEL_TYPES_FIELD)
-      ->setTargetEntityTypeId('entity_test_with_bundle')
-      ->setLabel(t('Enabled Content types'))
-      ->setRequired(FALSE)
-      ->setTranslatable(FALSE)
-      ->setCardinality(BundleFieldDefinition::CARDINALITY_UNLIMITED)
-      ->setSettings([
-        // TODO! this needs to move into Finders
-        'handler' => 'default',
-        'target_type' => 'entity_test_with_bundle',
-      ])
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayOptions('form', [
-        'type' => 'options_buttons',
-      ]);
-  }
- */
+
 }
