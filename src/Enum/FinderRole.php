ef5eb71e3087b61d6cfe8b061452b33d0f21228d
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-11-25 10:58:16 +0000

    docs

--- src/Enum/FinderRole.php
+++ src/Enum/FinderRole.php
@@ -8,12 +8,12 @@ namespace Drupal\finders\Enum;
 enum FinderRole: string {
 
   /**
-   * Nodes whose node type has this role show lists of entries.
+   * Bundles with this role show lists of entries.
    */
   case Channel = 'channel';
 
   /**
-   * Nodes whose node type has this role are entries shown in listings.
+   * Bundles with this role are entries shown in listings.
    */
   case Entries = 'entries';
 
